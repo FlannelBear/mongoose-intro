6177d08adbd7a990be4634c318e1b93ed6c018e2
Flannel Bear <josh.flannelbear@gmail.com>
2018-05-30 13:37:57 -0500

    started a book schema

--- server/routers/router.js
+++ server/routers/router.js
@@ -2,6 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 
+router.get('/', (req, res) => {
+
+});
+
+router.post('/', (req, res) => {
+
+});
+
+router.delete('/', (req, res) => {
+
+});
 
 
 module.exports = router;
